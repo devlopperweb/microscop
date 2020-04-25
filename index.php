1858d7732152e5fef4adf1c9bc5c7bb9dd36667e
Safwa9amar <45048385+Safwa9amar@users.noreply.github.com>
2020-04-25 18:45:33 +0200

    index.php
    image to binar text

---
+++ index.php
@@ -0,0 +1,17 @@
+<? $image = "https://static.arageek.com/wp-content/uploads/2019/02/mnm.jpg"; ?>
+<img src="<?=$image?>" height="100" width="100" ><br />
+<?
+ $im = imagecreatefromjpeg($image);
+ $dx = imagesx($im);
+ $dy = imagesy($im);
+ for($y = 0; $y < $dy; $y++) {
+ for($x=0; $x < $dx; $x++) {
+ $col = imagecolorat($im, $x, $y);
+ $rgb = imagecolorsforindex($im,$col);
+ printf("<font color=#%02x%02x%02x>()</font>",
+ $rgb['red'],$rgb['green'],$rgb['blue']);
+ }
+ echo "<br />\n";
+ }
+ imagedestroy($im);
+?>
